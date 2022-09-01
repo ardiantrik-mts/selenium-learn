db5fc95efd6e0dc5a7e391e5f1b9da425e09cc48
ardiantrik <ardiantrik@gmail.com>
2022-09-01 15:30:03 +0700

    add testNG login test

--- src/test/java/test/SauceDemoLoginPageTest.java
+++ src/test/java/test/SauceDemoLoginPageTest.java
@@ -2,29 +2,69 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pages.SauceDemoLoginPage;
 
 public class SauceDemoLoginPageTest {
-	private static WebDriver driver = null;
+	WebDriver driver = null;
+//	public SauceDemoLoginPage loginPage = new SauceDemoLoginPage(driver);
 	
-	public static void main(String[] args) {
-		sauceDemoLoginTest();
-	}
+//	public static void main(String[] args) {
+//		sauceDemoLoginTest();
+//	}
 	
-	public static void sauceDemoLoginTest() {
+	@BeforeTest
+	public void setUpTest() {
 		WebDriverManager.chromedriver().driverVersion("104.0.5112.79").setup();
 		driver = new ChromeDriver();
-		
-		SauceDemoLoginPage loginPage = new SauceDemoLoginPage(driver);
+	}
+	
+	@BeforeMethod
+	public void navigateSauceDemo() {
 		driver.get("https://google.com");
 		driver.navigate().to("https://saucedemo.com");
+	}
+	
+	@AfterTest
+	public void tearDownTest() {
+//		driver.close();
+		System.out.println("COMPLETED");
+	}
+	
+	@Test(priority = 1)
+	public void allEmptyFieldTest() {
+		SauceDemoLoginPage loginPage = new SauceDemoLoginPage(driver);
+		
+		loginPage.clickLogin();
+	}
+	
+	@Test(priority = 2)
+	public void usernameOnlyTest() {
+		SauceDemoLoginPage loginPage = new SauceDemoLoginPage(driver);
+		
+		loginPage.setUsername("standard_user");
+		loginPage.clickLogin();
+	}
+	
+	@Test(priority = 3)
+	public void wrongPasswordTest() {
+		SauceDemoLoginPage loginPage = new SauceDemoLoginPage(driver);
+		
+		loginPage.setUsername("standard_user");
+		loginPage.setPassword("123");
+		loginPage.clickLogin();
+	}
+	
+	@Test(priority = -1)
+	public void successLoginTest() {
+		SauceDemoLoginPage loginPage = new SauceDemoLoginPage(driver);
 		
 		loginPage.setUsername("standard_user");
 		loginPage.setPassword("secret_sauce");
 		loginPage.clickLogin();
-//		driver.close();
+
 	}
 
 }
